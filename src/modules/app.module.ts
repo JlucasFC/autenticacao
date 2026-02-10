fa5b12a413f27584758f041c287cdffb7e17ca2e
jlucas <jlucasfchaves@gmail.com>
2026-02-10 15:15:07 -0400

    config database

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CompaniesModule } from './companies/companies.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    CompaniesModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: process.env['DATABASE_TYPE'] as 'postgres',
+      host: process.env['DATABASE_HOST'],
+      port: Number(process.env['DATABASE_PORT']),
+      username: process.env['DATABASE_USERNAME'],
+      password: process.env['DATABASE_PASSOWRD'],
+      database: process.env['DATABASE_DB'],
+      autoLoadEntities: Boolean(process.env['DATABASE_AUTOLOADENTITIES']),
+      synchronize: Boolean(process.env['DATABASE_SYNCHRONIZE']),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
